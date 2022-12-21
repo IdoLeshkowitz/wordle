e07e82c539a80be82434ebb0ab739e683ce8c326
IdoLeshkowitz <idoleshkowitz@gmail.com>
2022-12-21 18:06:29 +0200

    focus

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -3,29 +3,39 @@ import {GameContext} from "../pages/Game";
 
 const Display = ({children}: any) => {
     const {state} = useContext(GameContext);
-    const {guessedWords} = state;
-    const extractDisplayGridFromState = () => {
-        let displayGrid: string[] = Array(guessedWords.length * 5).fill('');
-        let counter = 0; // to keep track of the index of the displayGrid
-        for (let guessedWord of guessedWords) {
-            for (let letter of guessedWord) {
-                displayGrid[counter] = letter;
-                counter++;
+    const {guessedWords, amountOfGuesses, guessedWordLength} = state;
+    const getDisplayGrid = (guessedWords: string[]) => {
+        const output = Array(amountOfGuesses * guessedWordLength).fill('');
+        for (let i = 0; i < guessedWords.length; i++) {
+            for (let j = 0; j < guessedWords[i].length; j++) {
+                output[i * guessedWordLength + j] = guessedWords[i][j];
             }
         }
-        return displayGrid;
+        return output;
     }
-    const displayGrid = extractDisplayGridFromState();
+
+    const getFocusedItemIndex = (displayGrid: string[]) => {
+        const focusedItemIndex = displayGrid.findIndex((item) => item === '');
+        return focusedItemIndex;
+    }
+    const getDisplayItemClass = (index: number, focusedItemIndex: number) => {
+        if (index === focusedItemIndex) {
+            return 'display-item focused';
+        }
+        return 'display-item';
+    }
+    const displayGrid = getDisplayGrid(guessedWords);
+    const focusedItemIndex = getFocusedItemIndex(displayGrid);
     return (
         <div className="display">
-            {displayGrid.map((displayItem, index ) =>
-                <div className="display-item"
-                     key={index}>
+            {displayGrid.map((displayItem, index) =>
+                <div className={getDisplayItemClass(index, focusedItemIndex)}
+                     key={index}
+                >
                     {displayItem}
-                </div>)
-            }
+                </div>
+            )}
         </div>
-
     )
 }
 export default Display;
--- src/gameLogic/GameState.ts
+++ src/gameLogic/GameState.ts
@@ -3,11 +3,15 @@ export interface GameState {
     guessedWords: string [];
     currentWordIndex: number;
     readyToGuess: boolean;
+    amountOfGuesses : number;
+     guessedWordLength: number;
 }
 
 export const gameInitialState: GameState = {
     helpModalActive: true,
-    guessedWords: ["","","","",""],
     currentWordIndex: 0,
     readyToGuess: true,
-}
+    amountOfGuesses: 5,
+    guessedWordLength: 5,
+    guessedWords: [],
+};
