0bb0090d0ba7ccfd28606b21ea8dc09cabc37974
IdoLeshkowitz <idoleshkowitz@gmail.com>
2022-12-31 17:02:05 +0200

    tests

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,7 +1,13 @@
+import {describe, expect, test} from "vitest";
+import request from "supertest";
 import app from "./server";
-
-test("GET /",()=>{
-    app.get("/",(req,res)=>{
+describe("user validation", () => {
+    const validateUser = ({userName,password} : {userName: string, password: string}) => request(app).post("/users").send({userName,password});
+    const validUser = {userName: "John Doe", password: "1234"};
+    const invalidUser = {userName: "John Doe", password: "123"};
+    test("valid user", async () => {
+        const res = await validateUser(validUser);
         expect(res.status).toBe(200);
+        expect(res.body).toEqual(validUser);
     });
-})
+});
--- src/server.ts
+++ src/server.ts
@@ -11,19 +11,19 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello Ws!');
 });
-// app.route('/users')
-//     //get user object with username and password check if exists in users array
-//     //if exists return user object
-//     //if not return error
-//     .post((req, res) => {
-//         const userToCheck = req.body;
-//         const user = users.find(user => user.name === userToCheck.name);
-//         if (user) {
-//             res.status(200).send(user);
-//         }
-//         else {
-//             res.status(404).send('user not found');
-//         }
-//     });
+app.route('/users')
+    //get user object with username and password check if exists in users array
+    //if exists return user object
+    //if not return error
+    .post((req, res) => {
+        const userToCheck = req.body;
+        const user = users.find(user => user.name === userToCheck.name);
+        if (user) {
+            res.status(200).send(user);
+        }
+        else {
+            res.status(404).send('user not found');
+        }
+    });
 
 export default app;
